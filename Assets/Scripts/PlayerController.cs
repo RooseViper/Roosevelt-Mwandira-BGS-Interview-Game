aa34f0f06fd926b2b24d68bdcc8474b5608a84bf
rmwandira <roosemwandira@gmail.com>
2022-12-17 17:41:35 +0200

    Feature: Added Player movement animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,10 +11,12 @@ public class PlayerController : MonoBehaviour
     /// </summary>
     private Vector2 _movement;
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -28,7 +30,13 @@ public class PlayerController : MonoBehaviour
     {
         Move();
     }
-    
+
+    private void FixedUpdate()
+    {
+        //Position moved in FixedUpdate so that it gives more accurate results since the Players rigidbody affects Physics 
+        _rigidbody.MovePosition(_rigidbody.position + _movement * speed * Time.fixedDeltaTime); 
+    }
+
     /// <summary>
     /// Allows the Player to Move
     /// </summary>
@@ -37,6 +45,10 @@ public class PlayerController : MonoBehaviour
         _movement.x = Input.GetAxisRaw("Horizontal");
         _movement.y = Input.GetAxisRaw("Vertical");
         
-        _rigidbody.MovePosition(_rigidbody.position + _movement * speed * Time.fixedDeltaTime); 
+        _animator.SetFloat("Horizontal", _movement.x);
+        _animator.SetFloat("Vertical", _movement.y);
+        _animator.SetFloat("Speed", _movement.sqrMagnitude);
     }
+    
+    
 }
