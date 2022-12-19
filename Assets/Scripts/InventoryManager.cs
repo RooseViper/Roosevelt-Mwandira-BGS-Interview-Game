0605c86c4eed3376ed471771bf77b9710f9f2288
rmwandira <roosemwandira@gmail.com>
2022-12-20 01:37:20 +0200

    Feature: Added Inventory window

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -9,13 +9,14 @@ using UnityEngine.UI;
 /// </summary>
 public class InventoryManager : MonoBehaviour
 {
-    public PlayerController playerController;
+    public OutFitManager outFitManager;
     public Text generalDisplayBalanceText;
     public float balance;
+    public RectTransform inventoryItemsParent;
     public static InventoryManager Instance => _instance;
     private static InventoryManager _instance;
-   
-    
+    [HideInInspector]
+    public InventoryItem[] inventoryItems;
     private void Awake()
     {
         _instance = this;
@@ -25,5 +26,49 @@ public class InventoryManager : MonoBehaviour
     void Start()
     {
         generalDisplayBalanceText.text = "" + balance;
+        inventoryItems = inventoryItemsParent.GetComponentsInChildren<InventoryItem>();
+        foreach (InventoryItem inventoryItem in inventoryItems)
+        {
+            inventoryItem.Clear();
+        }
+    }
+
+
+    public void OpenInventory()
+    {
+        ShopItem[] shopItems = ShopManager.Instance.shopItemsPlayerParent.GetComponentsInChildren<ShopItem>();
+        List<ShopItem> newShopItems = new List<ShopItem>();
+        foreach (ShopItem shopItem in shopItems)
+        {
+            if (!string.IsNullOrEmpty(shopItem.itemName))
+            {
+                newShopItems.Add(shopItem);
+            }
+        }
+        for (int i = 0; i < newShopItems.Count; i++)
+        {
+            if (newShopItems[i].itemName != null)
+            {
+                inventoryItems[i].image.enabled = true;
+                inventoryItems[i].itemName = newShopItems[i].itemName;
+                inventoryItems[i].outfit = newShopItems[i].outfit;
+                if (newShopItems[i].price <= 0f)
+                {
+                    inventoryItems[i].isOutfit = true;
+                }
+                inventoryItems[i].image.sprite = newShopItems[i].sprite;
+                inventoryItems[i].quantityText.text = ""+ newShopItems[i].quantity;
+            }
+        }
+        GameManager.Instance.OpenCloseCanvas("CanvasInventory", true);
+    }
+
+    public void CloseInventory()
+    {
+        foreach (InventoryItem inventoryItem in inventoryItems)
+        {
+            inventoryItem.Clear();
+        }
+        GameManager.Instance.OpenCloseCanvas("CanvasInventory", false);
     }
 }
